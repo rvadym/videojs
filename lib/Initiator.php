1cbb297c4bafe4fef7f2a98be089f0eea3c4bf5b
vadym <vadym@xxx.(none)>
2014-01-31 09:10:48 +0200

    initial commit

---
+++ lib/Initiator.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: vadym
+ * Date: 1/31/14
+ * Time: 9:06 AM
+ * To change this template use File | Settings | File Templates.
+ */
+namespace videojs;
+class Initiator extends \AbstractController {
+    function init() {
+        parent::init();
+    }
+}
